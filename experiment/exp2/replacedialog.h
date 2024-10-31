0e7eff6832731f0eb05e0ece8bab044eae03956e
skmgt <skmgt1024@outlook.com>
2024-10-31 10:47:23 +0800

    替换功能实现

--- experiment/exp2/replacedialog.h
+++ experiment/exp2/replacedialog.h
@@ -2,7 +2,8 @@
 #define REPLACEDIALOG_H
 
 #include <QDialog>
-
+#include <QPlainTextEdit>
+#include <QMessageBox>
 namespace Ui {
 class ReplaceDialog;
 }
@@ -12,11 +13,21 @@ class ReplaceDialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit ReplaceDialog(QWidget *parent = nullptr);
+    explicit ReplaceDialog(QWidget *parent = nullptr,QPlainTextEdit * textedit=nullptr);
     ~ReplaceDialog();
+
+private slots:
+    void on_btnFindNext_clicked();
+
+    void on_btnReplace_clicked();
+
+    void on_btnReplaceAll_clicked();
+
+    void on_btnCancel_clicked();
 
 private:
     Ui::ReplaceDialog *ui;
+    QPlainTextEdit *pText;
 };
 
 #endif // REPLACEDIALOG_H
