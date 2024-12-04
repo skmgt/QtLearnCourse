92ae17122252c52f1ae5bf746f708481d72404a5
skmgt <skmgt1024@outlook.com>
2024-12-04 11:11:50 +0800

    服务器端通讯实现

---
+++ experiment/read-lab4/ChatServer/serverworker.cpp
@@ -0,0 +1,50 @@
+#include "serverworker.h"
+#include <QDataStream>
+#include <QJsonObject>
+#include <QJsonDocument>
+
+ServerWorker::ServerWorker(QObject *parent)
+    : QObject{parent}
+{
+    m_serverSocket = new QTcpSocket(this);
+    connect(m_serverSocket,&QTcpSocket::readyRead,this,&ServerWorker::onReadyRead);
+}
+
+bool ServerWorker::setSocketDescriptor(qintptr socketDescriptor)
+{
+    return m_serverSocket->setSocketDescriptor(socketDescriptor);
+}
+
+void ServerWorker::onReadyRead()
+{
+    QByteArray jsonData;
+    QDataStream socketStream(m_serverSocket);
+    socketStream.setVersion(QDataStream::Qt_6_7);
+
+    for(;;){
+        socketStream.startTransaction();
+        socketStream >> jsonData;
+        if(socketStream.commitTransaction()){
+            emit logMessage(QString::fromUtf8(jsonData));
+            sendMessage("I recieved message");
+        } else{
+            break;
+        }
+    }
+}
+
+void ServerWorker::sendMessage(const QString &text, const QString &type)
+{
+    if(m_serverSocket->state() != QAbstractSocket::ConnectedState)
+        return;
+    if(!text.isEmpty()){
+        QDataStream serverStream(m_serverSocket);
+        serverStream.setVersion(QDataStream::Qt_6_7);
+
+        QJsonObject message;
+        message["type"] = type;
+        message["text"] = text;
+
+        serverStream << QJsonDocument(message).toJson();
+    }
+}
